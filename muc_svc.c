5e6612f0f4e0a0dc3d0f6635df9fd2374ce11fd6
Jason Hrycay <jason.hrycay@motorola.com>
2015-08-07 10:18:12 -0500

    muc_svc: Create a platform dev and DL device
    Change the muc_svc to register and create a platform device. In the
    probe, we'll initially register as a new Data Link device so that we can
    eventually send SVC protocol messages to the AP.
    
    Change-Id: I1e1dbf5c1798d23d0b6bf6bcecbe87337f8d7066
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>

--- muc_svc.c
+++ muc_svc.c
@@ -19,11 +19,16 @@
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/spi/spi.h>
+#include <linux/platform_device.h>
 
-#include "endo.h"
 #include "greybus.h"
 
 #include "muc_svc.h"
+#include "mods_nw.h"
+
+struct muc_svc_data {
+	struct mods_dl_device *dld;
+};
 
 #define HP_BASE_SIZE      (sizeof(struct svc_msg_header) + 2)
 #define LU_PAYLOAD_SIZE   (sizeof(struct svc_function_unipro_management))
@@ -59,12 +64,97 @@ void muc_svc_detach(struct greybus_host_device *hd)
 }
 EXPORT_SYMBOL(muc_svc_detach);
 
-int __init muc_svc_init(void)
+static int
+muc_svc_msg_send(struct mods_dl_device *dld, uint8_t *buf, size_t len)
 {
 	return 0;
 }
 
+static void muc_svc_msg_cancel(void *cookie)
+{
+	/* Should never happen */
+}
+
+static struct mods_dl_driver muc_svc_dl_driver = {
+	.dl_priv_size = sizeof(struct muc_svc_data),
+	.message_send = muc_svc_msg_send,
+	.message_cancel = muc_svc_msg_cancel,
+};
+
+static int muc_svc_probe(struct platform_device *pdev)
+{
+	struct muc_svc_data *dd;
+
+	dd = devm_kzalloc(&pdev->dev, sizeof(*dd), GFP_KERNEL);
+	if (!dd)
+		return -ENOMEM;
+
+	dd->dld = mods_create_dl_device(&muc_svc_dl_driver, &pdev->dev);
+	if (IS_ERR(dd->dld)) {
+		dev_err(&pdev->dev, "Failed to create mods DL device.\n");
+		return PTR_ERR(dd->dld);
+	}
+
+	platform_set_drvdata(pdev, dd);
+
+	return 0;
+}
+
+static int muc_svc_remove(struct platform_device *pdev)
+{
+	struct muc_svc_data *dd = platform_get_drvdata(pdev);
+
+	mods_remove_dl_device(dd->dld);
+
+	return 0;
+}
+
+static struct platform_driver muc_svc_driver = {
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = "muc_svc",
+	},
+	.probe = muc_svc_probe,
+	.remove  = muc_svc_remove,
+};
+
+static struct platform_device *muc_svc_device;
+
+int __init muc_svc_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&muc_svc_driver);
+	if (ret < 0) {
+		pr_err("muc_svc failed to register driver\n");
+		return ret;
+	}
+
+	muc_svc_device = platform_device_alloc("muc_svc", -1);
+	if (!muc_svc_device) {
+		ret = -ENOMEM;
+		pr_err("muc_svc failed to alloc device\n");
+		goto alloc_fail;
+	}
+
+	ret = platform_device_add(muc_svc_device);
+	if (ret) {
+		pr_err("muc_svc failed to add device: %d\n", ret);
+		goto add_fail;
+	}
+
+	return 0;
+
+add_fail:
+	platform_device_put(muc_svc_device);
+alloc_fail:
+	platform_driver_unregister(&muc_svc_driver);
+
+	return ret;
+}
+
 void __exit muc_svc_exit(void)
 {
-	/* nada */
+	platform_driver_unregister(&muc_svc_driver);
+	platform_device_unregister(muc_svc_device);
 }
