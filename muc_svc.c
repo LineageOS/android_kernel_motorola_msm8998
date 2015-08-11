42e61b104f45970ce386f44f233a54f1b501c480
Jason Hrycay <jason.hrycay@motorola.com>
2015-08-11 10:25:01 -0500

    muc_svc: Initial SVC to AP communication
    Add the necessary functionality to allow the SVC to talk to the AP in
    order to negotiate the SVC Protocol version and eventually the SVC_HELLO
    to configure the AP's interface ID and also the ENDO configuration. Once
    this step is complete, the SVC can then notify the AP about new
    interface hotplug events as they come in.
    
    Change-Id: I8e930598c88b2d6c38f95e9acc9c15103a14ce8c
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>

--- muc_svc.c
+++ muc_svc.c
@@ -28,55 +28,326 @@
 
 struct muc_svc_data {
 	struct mods_dl_device *dld;
+	atomic_t msg_num;
+	struct list_head operations;
+	struct platform_device *pdev;
 };
 
 /* XXX Move these into device tree? */
 #define MUC_SVC_AP_INTF_ID 1
 #define MUC_SVC_ENDO_ID 0x4755
 
-#define HP_BASE_SIZE      (sizeof(struct svc_msg_header) + 2)
-#define LU_PAYLOAD_SIZE   (sizeof(struct svc_function_unipro_management))
-#define LU_MSG_SIZE       (sizeof(struct svc_msg_header) + LU_PAYLOAD_SIZE)
+#define MUC_SVC_RESPONSE_TYPE 0
 
-static void send_hot_plug(struct greybus_host_device *hd, int iid)
-{
-	pr_info("SVC -> AP hotplug event (plug) sent\n");
-}
-
-/* Mock SVC message to the AP */
-static void send_hot_unplug(struct greybus_host_device *hd, int iid)
-{
-	printk("%s: SVC->AP hotplug event (unplug) sent\n", __func__);
-}
-
-static void send_link_up(struct greybus_host_device *hd, int iid, int did)
-{
-	pr_info("SVC -> AP Link Up (%d:%d) message sent\n", iid, did);
-}
-
+#define SVC_MSG_TIMEOUT 500
 
 void muc_svc_attach(struct greybus_host_device *hd)
 {
-	send_hot_plug(hd, 1);
-	send_link_up(hd, 1, 2);
 }
 EXPORT_SYMBOL(muc_svc_attach);
 
 void muc_svc_detach(struct greybus_host_device *hd)
 {
-	send_hot_unplug(hd, 1);
 }
 EXPORT_SYMBOL(muc_svc_detach);
 
-static int muc_svc_probe_ap(uint8_t ap_intf_id)
+static struct gb_message *svc_gb_msg_alloc(u8 type, size_t payload_size)
 {
+	struct gb_message *msg;
+	struct gb_operation_msg_hdr *hdr;
+	size_t message_size = payload_size + sizeof(*hdr);
+
+	msg = kzalloc(sizeof(*msg), GFP_KERNEL);
+	if (!msg)
+		return NULL;
+
+	msg->buffer = kzalloc(message_size, GFP_KERNEL);
+	if (!msg) {
+		kfree(msg);
+		return NULL;
+	}
+
+	hdr = msg->buffer;
+	hdr->size = cpu_to_le16(message_size);
+	hdr->operation_id = 0;
+	hdr->type = type;
+	hdr->result = 0;
+
+	msg->header = hdr;
+	msg->payload = payload_size ? hdr + 1 : NULL;
+	msg->payload_size = payload_size;
+
+	return msg;
+}
+
+static void svc_gb_msg_free(struct gb_message *msg)
+{
+	if (!msg)
+		return;
+	kfree(msg->buffer);
+	kfree(msg);
+}
+
+struct svc_op {
+	struct list_head entry;
+	struct completion completion;
+	struct gb_message *request;
+	struct gb_message *response;
+	u16 msg_id;
+};
+
+static inline struct muc_svc_data *dld_get_dd(struct mods_dl_device *dld)
+{
+	return (struct muc_svc_data *)dld->dl_priv;
+}
+
+static inline size_t get_gb_msg_size(struct gb_message *msg)
+{
+	return sizeof(*msg->header) + msg->payload_size;
+}
+
+static struct svc_op *svc_find_op(struct muc_svc_data *dd, uint16_t id)
+{
+	struct svc_op *e, *tmp;
+
+	list_for_each_entry_safe(e, tmp, &dd->operations, entry)
+		if (e->msg_id == id)
+			return e;
+
+	return NULL;
+}
+
+/* Route a gb_message to the mods_nw layer, adding the necessary
+ * envelope that it understands.
+ */
+static int
+svc_route_msg(struct mods_dl_device *dld, uint8_t src_cport,
+		uint8_t dest_cport, struct gb_message *msg)
+{
+	struct muc_msg *m;
+	size_t muc_payload = get_gb_msg_size(msg);
+	size_t msg_size = muc_payload + sizeof(m->hdr);
+
+	m = kmalloc(msg_size, GFP_KERNEL);
+	if (!m)
+		return -ENOMEM;
+
+	memcpy(m->gb_msg, msg->buffer, muc_payload);
+	m->hdr.size = msg->header->size;
+	m->hdr.dest_cport = dest_cport;
+	m->hdr.src_cport = src_cport;
+
+	mods_data_rcvd(dld, (uint8_t *)m);
+	kfree(m);
+
+	return 0;
+}
+
+/* Handle the incoming greybus message and complete the waiting thread */
+/* XXX This only accounts for responses.... need to determine if its response
+ * or a brand new message to the SVC (even possible? AP set route, maybe).
+ */
+static int
+svc_gb_msg_recv(struct mods_dl_device *dld, uint8_t *data,
+		size_t msg_size, uint8_t cport)
+{
+	struct muc_svc_data *dd = dld_get_dd(dld);
+	struct svc_op *op;
+	size_t payload_size;
+	struct gb_operation_msg_hdr hdr;
+
+	payload_size = msg_size - sizeof(struct gb_operation_msg_hdr);
+	if (msg_size < sizeof(hdr)) {
+		dev_err(&dd->pdev->dev, "msg size too small: %zu\n", msg_size);
+		return -EINVAL;
+	}
+
+	memcpy(&hdr, data, sizeof(hdr));
+
+	if (hdr.type & GB_MESSAGE_TYPE_RESPONSE) {
+		op = svc_find_op(dd, le16_to_cpu(hdr.operation_id));
+		if (!op) {
+			dev_err(&dd->pdev->dev, "OpID: %d unknown\n",
+				le16_to_cpu(hdr.operation_id));
+			return -EINVAL;
+		}
+
+		op->response = svc_gb_msg_alloc(MUC_SVC_RESPONSE_TYPE, payload_size);
+		if (!op->response)
+			return -ENOMEM;
+
+		memcpy(op->response->header, data, msg_size);
+		complete(&op->completion);
+	} else {
+		/* This is a new incoming request! */
+	}
+
+	return 0;
+}
+
+/* Send a message out the specified CPORT and wait for a response */
+static struct gb_message *
+svc_gb_msg_send_sync(struct mods_dl_device *dld, uint8_t *data, uint8_t type,
+		size_t payload_size, uint8_t src_cport, uint8_t dest_cport)
+{
+	struct muc_svc_data *dd = dld_get_dd(dld);
+	struct svc_op *op;
+	struct gb_message *msg;
+	int ret;
+	uint16_t cycle;
+
+	op = kzalloc(sizeof(*op), GFP_KERNEL);
+	if (!op)
+		return ERR_PTR(-ENOMEM);
+
+	msg = svc_gb_msg_alloc(type, payload_size);
+	if (!msg) {
+		ret = -ENOMEM;
+		goto gb_msg_alloc;
+	}
+
+	cycle = (u16)atomic_inc_return(&dd->msg_num);
+	op->msg_id = cycle % U16_MAX + 1;
+	init_completion(&op->completion);
+
+	msg->header->operation_id = cpu_to_le16(op->msg_id);
+	if (payload_size)
+		memcpy(msg->payload, data, payload_size);
+	op->request = msg;
+
+	list_add_tail(&op->entry, &dd->operations);
+
+	/* Send to NW Routing Layer */
+	ret = svc_route_msg(dld, src_cport, dest_cport, msg);
+	if (ret) {
+		dev_err(&dd->pdev->dev, "failed sending svc msg: %d\n", ret);
+		goto remove_op;
+	}
+
+	ret = wait_for_completion_interruptible_timeout(&op->completion,
+					msecs_to_jiffies(SVC_MSG_TIMEOUT));
+	if (ret <= 0) {
+		dev_err(&dd->pdev->dev, "svc msg response timeout\n");
+		if (!ret)
+			ret = -ETIMEDOUT;
+		goto remove_op;
+	}
+
+	/* Remove and free the request */
+	list_del(&op->entry);
+	svc_gb_msg_free(op->request);
+	op->request = NULL;
+
+	msg = op->response;
+	kfree(op);
+
+	/* XXX Check result here? */
+	if (msg->header->result) {
+		svc_gb_msg_free(op->response);
+		return ERR_PTR(-EINVAL);
+	}
+
+	return msg;
+
+remove_op:
+	list_del(&op->entry);
+	svc_gb_msg_free(op->request);
+gb_msg_alloc:
+	kfree(op);
+
+	return ERR_PTR(ret);
+}
+
+static int muc_svc_version_check(struct mods_dl_device *dld)
+{
+	struct muc_svc_data *dd = dld_get_dd(dld);
+	struct gb_protocol_version_response *ver;
+	struct gb_message *msg;
+
+	ver = kmalloc(sizeof(*ver), GFP_KERNEL);
+	if (!ver)
+		return -ENOMEM;
+
+	ver->major = GB_SVC_VERSION_MAJOR;
+	ver->minor = GB_SVC_VERSION_MINOR;
+
+	msg = svc_gb_msg_send_sync(dld, (uint8_t *)ver, GB_SVC_TYPE_PROTOCOL_VERSION,
+				sizeof(*ver), 0, 0);
+	if (IS_ERR(msg)) {
+		dev_err(&dd->pdev->dev, "Failed to get VERSION from AP\n");
+		kfree(ver);
+		return PTR_ERR(msg);
+	}
+
+	kfree(ver);
+	ver = msg->payload;
+
+	/* XXX We could check versions... */
+	dev_info(&dd->pdev->dev, "VERSION: %hhu.%hhu\n",
+		ver->major, ver->minor);
+
 	return 0;
 }
 
 static int
+muc_svc_hello_req(struct mods_dl_device *dld, uint8_t ap_intf_id)
+{
+	struct muc_svc_data *dd = dld_get_dd(dld);
+	struct gb_message *msg;
+	struct gb_svc_hello_request *hello;
+
+	hello = kmalloc(sizeof(*hello), GFP_KERNEL);
+	if (!hello)
+		return -ENOMEM;
+
+	/* Send the endo id and the AP's interface ID */
+	hello->endo_id = cpu_to_le16(MUC_SVC_ENDO_ID);
+	hello->interface_id = ap_intf_id;
+
+	msg = svc_gb_msg_send_sync(dld, (uint8_t *)hello, GB_SVC_TYPE_SVC_HELLO,
+				sizeof(*hello), 0, 0);
+	if (IS_ERR(msg)) {
+		dev_err(&dd->pdev->dev, "Failed to send HELLO to AP\n");
+		kfree(hello);
+		return PTR_ERR(msg);
+	}
+
+	svc_gb_msg_free(msg);
+	kfree(hello);
+
+	return 0;
+}
+
+static int
+muc_svc_probe_ap(struct mods_dl_device *dld, uint8_t ap_intf_id)
+{
+	struct muc_svc_data *dd = dld_get_dd(dld);
+	int ret;
+
+	ret = muc_svc_version_check(dld);
+	if (ret) {
+		dev_err(&dd->pdev->dev, "SVC version check failed\n");
+		return ret;
+	}
+
+	ret = muc_svc_hello_req(dld, ap_intf_id);
+	if (ret) {
+		dev_err(&dd->pdev->dev, "SVC HELLO failed\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+/* Handle the muc_msg and strip out its envelope to pass along the
+ * actual gb_message we're interested in.
+ */
+static int
 muc_svc_msg_send(struct mods_dl_device *dld, uint8_t *buf, size_t len)
 {
-	return 0;
+	struct muc_msg *m = (struct muc_msg *)buf;
+
+	return svc_gb_msg_recv(dld, m->gb_msg, m->hdr.size, m->hdr.dest_cport);
 }
 
 static void muc_svc_msg_cancel(void *cookie)
@@ -104,17 +375,28 @@ static int muc_svc_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to create mods DL device.\n");
 		return PTR_ERR(dd->dld);
 	}
+	dd->dld->dl_priv = dd;
+
+	dd->pdev = pdev;
+	atomic_set(&dd->msg_num, 1);
+	INIT_LIST_HEAD(&dd->operations);
 
 	platform_set_drvdata(pdev, dd);
 
 	/* We are a single entity, AP is always attached */
-	ret = muc_svc_probe_ap(MUC_SVC_AP_INTF_ID);
+	ret = muc_svc_probe_ap(dd->dld, MUC_SVC_AP_INTF_ID);
 	if (ret) {
 		dev_err(&pdev->dev, "Failed to probe the AP: %d\n", ret);
-		return ret;
+		goto ap_probe_fail;
 	}
-
 	return 0;
+
+ap_probe_fail:
+	mods_remove_dl_device(dd->dld);
+
+	platform_set_drvdata(pdev, NULL);
+
+	return ret;
 }
 
 static int muc_svc_remove(struct platform_device *pdev)
