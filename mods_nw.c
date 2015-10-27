f2bfb8114d8bea389641600789dd0c875787ce37
Jason Hrycay <jason.hrycay@motorola.com>
2015-10-27 10:38:41 -0500

    mods_nw: Use radix tree for cport entries
    Migrate the static, linear array of CPORTs per interface into a sparse
    version with a radix tree. This eliminates the wasted, empty entries and
    removes our hardcoded port restrictions.
    
    Change-Id: I47353120b5d6bbb42a8580b7432f574a8aa80fb0
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.mot.com/794768
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: James Wylder <jwylder@motorola.com>

--- mods_nw.c
+++ mods_nw.c
@@ -39,8 +39,8 @@ struct dest_entry {
 };
 
 struct cport_set {
-	struct mods_dl_device	*dev;
-	struct dest_entry	dest[CONFIG_CPORT_ID_MAX];
+	struct mods_dl_device *dev;
+	struct radix_tree_root  tree;
 };
 
 static LIST_HEAD(mods_nw_filters);
@@ -116,6 +116,7 @@ void mods_nw_add_dl_device(struct mods_dl_device *mods_dev)
 		goto unlock;
 
 	new->dev = mods_dev;
+	INIT_RADIX_TREE(&new->tree, GFP_KERNEL);
 	radix_tree_insert(&nw_interfaces, mods_dev->intf_id, new);
 
 unlock:
@@ -148,22 +149,52 @@ int mods_nw_add_route(u8 from_intf, u16 from_cport, u8 to_intf, u16 to_cport)
 	struct cport_set *to_cset;
 	uint8_t protocol;
 	bool filter = false;
-
-	BUG_ON(from_cport >= CONFIG_CPORT_ID_MAX);
-	BUG_ON(to_cport >= CONFIG_CPORT_ID_MAX);
+	struct dest_entry *from_entry;
+	struct dest_entry *to_entry;
+	bool alloc_from = false;
+	bool alloc_to = false;
 
 	from_cset = radix_tree_lookup(&nw_interfaces, from_intf);
 	to_cset = radix_tree_lookup(&nw_interfaces, to_intf);
 
 	if (!from_cset || !to_cset) {
-		pr_err("Unable to add route %u:%u -> %u:%u\n",
+		pr_err("Unable to find cset %u:%u -> %u:%u\n",
 			from_intf, from_cport, to_intf, to_cport);
 		return -ENODEV;
 	}
 
-	/* Set the destination port and device */
-	from_cset->dest[from_cport].cport = to_cport;
-	from_cset->dest[from_cport].dev = to_cset->dev;
+	/* Find existing entries which might have been created on the
+	 * first route direction. Both entries need to exist for filter
+	 * to be configured.
+	 */
+	from_entry = radix_tree_lookup(&from_cset->tree, from_cport);
+	if (!from_entry) {
+		from_entry = kzalloc(sizeof(*from_entry), GFP_KERNEL);
+		if (!from_entry) {
+			err = -ENOMEM;
+			goto cleanup;
+		}
+		mutex_lock(&list_lock);
+		radix_tree_insert(&from_cset->tree, from_cport, from_entry);
+		mutex_unlock(&list_lock);
+		alloc_from = true;
+	}
+
+	to_entry = radix_tree_lookup(&to_cset->tree, to_cport);
+	if (!to_entry) {
+		to_entry = kzalloc(sizeof(*to_entry), GFP_KERNEL);
+		if (!to_entry) {
+			err = -ENOMEM;
+			goto cleanup;
+		}
+		mutex_lock(&list_lock);
+		radix_tree_insert(&to_cset->tree, to_cport, to_entry);
+		mutex_unlock(&list_lock);
+		alloc_to = true;
+	}
+
+	from_entry->cport = to_cport;
+	from_entry->dev = to_cset->dev;
 
 	/* If there is no protocol handler, we are done */
 	if (!from_cset->dev->drv->get_protocol)
@@ -174,41 +205,62 @@ int mods_nw_add_route(u8 from_intf, u16 from_cport, u8 to_intf, u16 to_cport)
 	if (err) {
 		pr_warn("Unable to get a protocol for %d:%d\n",
 				from_intf, from_cport);
-		return err;
+		goto cleanup;
 	}
 
 	/* Look for previously installed filters */
 	filter = _mods_nw_filter_present(protocol);
 
 	/* Save the protocol and filter status */
-	from_cset->dest[from_cport].protocol_valid = true;
-	from_cset->dest[from_cport].protocol_id = protocol;
-	from_cset->dest[from_cport].filter = filter;
+	from_entry->protocol_valid = true;
+	from_entry->protocol_id = protocol;
+	from_entry->filter = filter;
 
-	to_cset->dest[to_cport].protocol_id = protocol;
-	to_cset->dest[to_cport].protocol_valid = true;
-	to_cset->dest[to_cport].filter = filter;
+	to_entry->protocol_id = protocol;
+	to_entry->protocol_valid = true;
+	to_entry->filter = filter;
 
 	return 0;
+
+cleanup:
+	mutex_lock(&list_lock);
+	if (alloc_to) {
+		radix_tree_delete(&to_cset->tree, to_cport);
+		kfree(to_entry);
+	}
+	if (alloc_from) {
+		radix_tree_delete(&from_cset->tree, from_cport);
+		kfree(from_entry);
+	}
+	mutex_unlock(&list_lock);
+
+	return err;
 }
 
 void mods_nw_del_route(u8 from_intf, u16 from_cport, u8 to_intf, u16 to_cport)
 {
 	struct cport_set *from_cset;
+	struct dest_entry *entry;
 
-	BUG_ON(from_cport >= CONFIG_CPORT_ID_MAX);
-	BUG_ON(to_cport >= CONFIG_CPORT_ID_MAX);
-
+	mutex_lock(&list_lock);
 	from_cset = radix_tree_lookup(&nw_interfaces, from_intf);
-	if (from_cset)
-		memset(&from_cset->dest[from_cport], 0,
-			sizeof(struct dest_entry));
+	if (!from_cset)
+		goto unlock;
+
+	entry = radix_tree_lookup(&from_cset->tree, from_cport);
+	if (entry) {
+		radix_tree_delete(&from_cset->tree, from_cport);
+		kfree(entry);
+	}
+
+unlock:
+	mutex_unlock(&list_lock);
 }
 
 int mods_nw_switch(struct mods_dl_device *from, uint8_t *msg, size_t len)
 {
 	struct muc_msg *mm;
-	struct dest_entry dest;
+	struct dest_entry *dest;
 	int err = -ENODEV;
 	struct cport_set *route;
 
@@ -225,29 +277,24 @@ int mods_nw_switch(struct mods_dl_device *from, uint8_t *msg, size_t len)
 		err = -EINVAL;
 		goto out;
 	}
-	if (le16_to_cpu(mm->hdr.cport) >= CONFIG_CPORT_ID_MAX) {
-		dev_err(from->dev, "Attempt to send on invalid cport\n");
-		err = -EINVAL;
-		goto out;
-	}
 
-	dest = route->dest[le16_to_cpu(mm->hdr.cport)];
-	if (!dest.dev) {
+	dest = radix_tree_lookup(&route->tree, le16_to_cpu(mm->hdr.cport));
+	if (!dest) {
 		dev_err(from->dev, "No route for %u:%u\n",
 				from->intf_id, le16_to_cpu(mm->hdr.cport));
 		goto out;
 	}
 
-	mm->hdr.cport = cpu_to_le16(dest.cport);
+	mm->hdr.cport = cpu_to_le16(dest->cport);
 
 	/* Try to apply any filter installed, or run standard message
 	 * send if no filter was present. A filter can also choose
 	 * to allow the message to continue to pass through with this
 	 * error code.
 	 */
-	err = _mods_nw_apply_filter(&dest, dest.dev, msg, len);
+	err = _mods_nw_apply_filter(dest, dest->dev, msg, len);
 	if (err == -ENOENT)
-		err = dest.dev->drv->message_send(dest.dev, msg, len);
+		err = dest->dev->drv->message_send(dest->dev, msg, len);
 
 out:
 	return err;
@@ -255,18 +302,21 @@ out:
 static void _set_filter(uint8_t protocol, bool value)
 {
 	struct radix_tree_iter rt_iter;
+	struct radix_tree_iter cp_iter;
 	void **rt_slot;
+	void **cp_slot;
 	struct cport_set *route;
-	int cport;
+	struct dest_entry *dest;
 
 	radix_tree_for_each_slot(rt_slot, &nw_interfaces, &rt_iter, 0) {
 		route = radix_tree_deref_slot(rt_slot);
 
-		for (cport = 0; cport < CONFIG_CPORT_ID_MAX; cport++) {
-			if (!route->dest[cport].protocol_valid)
+		radix_tree_for_each_slot(cp_slot, &route->tree, &cp_iter, 0) {
+			dest = radix_tree_deref_slot(cp_slot);
+			if (!dest->protocol_valid)
 				continue;
-			if (route->dest[cport].protocol_id == protocol)
-				route->dest[cport].filter = value;
+			if (dest->protocol_id == protocol)
+				dest->filter = value;
 		}
 	}
 }
