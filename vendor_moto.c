fa1fb2d97792eaa1b1a64a70a19be3afe12e1676
Greg Meiste <w30289@motorola.com>
2016-01-13 15:10:48 -0600

    vendor_moto: Support mods that have different sized dmesg buffers
    The initial 1000 byte max size for dmesg was chosen when the max
    greybus message size was 1K. Now that the max greybus size is 2K,
    allow modules to report larger dmesg buffers.
    
    The driver version should be set to 0.2, but the greybus core code
    only supports drivers with a version of 0.1. This should be fixed
    when supported.
    
    Change-Id: I71618ed6ef76cfe4e956086905c9e5cd8c6e4eaf
    Signed-off-by: Greg Meiste <w30289@motorola.com>
    Reviewed-on: http://gerrit.mot.com/813012
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jason Hrycay <jason.hrycay@motorola.com>

--- vendor_moto.c
+++ vendor_moto.c
@@ -1,7 +1,7 @@
 /*
  * Motorola specific driver for a Greybus module.
  *
- * Copyright 2015 Motorola Mobility, LLC.
+ * Copyright 2015-2016 Motorola Mobility, LLC.
  * Copyright 2014 Google Inc.
  * Copyright 2014 Linaro Ltd.
  *
@@ -20,53 +20,53 @@ struct gb_vendor_moto {
 	struct gb_connection *connection;
 	struct device *dev;
 	int minor;  /* vendor minor number */
+	uint16_t dmesg_size;
 };
 
 /* Version of the Greybus protocol we support */
 #define	GB_VENDOR_MOTO_VERSION_MAJOR		0x00
-#define	GB_VENDOR_MOTO_VERSION_MINOR		0x01
+#define	GB_VENDOR_MOTO_VERSION_MINOR		0x01  // FIXME: Set to 0x02 when supported
 
 /* Greybus Motorola vendor specific request types */
 #define	GB_VENDOR_MOTO_TYPE_GET_DMESG		0x02
 #define	GB_VENDOR_MOTO_TYPE_GET_LAST_DMESG	0x03
 #define	GB_VENDOR_MOTO_TYPE_GET_PWR_UP_REASON	0x04
+#define GB_VENDOR_MOTO_TYPE_GET_DMESG_SIZE	0x05
 
-/*
- * This is slightly less than max greybus payload size to allow for headers
- * and other overhead.
- */
-#define GB_VENDOR_MOTO_DMESG_SIZE           1000
-
-/* get (last) dmesg request has no payload */
-struct gb_vendor_moto_dmesg_response {
-	char	buf[GB_VENDOR_MOTO_DMESG_SIZE];
-};
+#define GB_VENDOR_MOTO_DEFAULT_DMESG_SIZE   1000
+#define GB_VENDOR_MOTO_VER_DMESG_SIZE       2
 
 /* power up reason request has no payload */
 struct gb_vendor_moto_pwr_up_reason_response {
 	__le32	reason;
 } __packed;
 
+/* get dmesg size request has no payload */
+struct gb_vendor_moto_get_dmesg_size_resp {
+	__le16  size;
+} __packed;
+
 static ssize_t do_get_dmesg(struct device *dev, struct device_attribute *attr,
 			    char *buf, int type)
 {
 	struct gb_vendor_moto *gb = dev_get_drvdata(dev);
-	struct gb_vendor_moto_dmesg_response *rsp;
+	char *dmesg;
 	int ret;
 
-	rsp = kmalloc(sizeof(struct gb_vendor_moto_dmesg_response), GFP_KERNEL);
-	if (!rsp)
+	dmesg = kmalloc(gb->dmesg_size, GFP_KERNEL);
+	if (!dmesg)
 		return -ENOMEM;
 
 	ret = gb_operation_sync(gb->connection, type,
-				NULL, 0, rsp, sizeof(*rsp));
+				NULL, 0, dmesg, gb->dmesg_size);
 	if (ret)
 		goto err;
 
-	ret = snprintf(buf, sizeof(*rsp), "%s\n", rsp->buf);
+	ret = scnprintf(buf, gb->dmesg_size, "%s\n", dmesg);
 
 err:
-	kfree(rsp);
+	kfree(dmesg);
+
 	return ret;
 }
 
@@ -124,6 +124,7 @@ static int gb_vendor_moto_connection_init(struct gb_connection *connection)
 	struct gb_vendor_moto *gb;
 	struct device *dev;
 	int retval;
+	struct gb_vendor_moto_get_dmesg_size_resp *rsp = NULL;
 
 	gb = kzalloc(sizeof(*gb), GFP_KERNEL);
 	if (!gb)
@@ -132,11 +133,30 @@ static int gb_vendor_moto_connection_init(struct gb_connection *connection)
 	gb->connection = connection;
 	connection->private = gb;
 
+	gb->dmesg_size = GB_VENDOR_MOTO_DEFAULT_DMESG_SIZE;
+	if (connection->module_minor >= GB_VENDOR_MOTO_VER_DMESG_SIZE) {
+		rsp = kmalloc(sizeof(struct gb_vendor_moto_get_dmesg_size_resp),
+			      GFP_KERNEL);
+		if (!rsp)
+			goto error;
+
+		retval = gb_operation_sync(gb->connection,
+					   GB_VENDOR_MOTO_TYPE_GET_DMESG_SIZE,
+					   NULL, 0, rsp, sizeof(*rsp));
+		if (retval)
+			goto err_rsp_free;
+
+		gb->dmesg_size = le16_to_cpu(rsp->size);
+	}
+
+	dev_info(&connection->bundle->dev, "module_minor=%d, dmesg_size=%d\n",
+		 connection->module_minor, gb->dmesg_size);
+
 	/* Create a device in sysfs */
 	gb->minor = ida_simple_get(&minors, 0, 0, GFP_KERNEL);
 	if (gb->minor < 0) {
 		retval = gb->minor;
-		goto error;
+		goto err_rsp_free;
 	}
 	dev = device_create(&vendor_class, &connection->bundle->dev,
 				MKDEV(0, 0), gb, "mod%d", gb->minor);
@@ -150,6 +170,8 @@ static int gb_vendor_moto_connection_init(struct gb_connection *connection)
 
 err_ida_remove:
 	ida_simple_remove(&minors, gb->minor);
+err_rsp_free:
+	kfree(rsp);
 error:
 	kfree(gb);
 	return retval;
@@ -167,8 +189,8 @@ static void gb_vendor_moto_connection_exit(struct gb_connection *connection)
 static struct gb_protocol vendor_moto_protocol = {
 	.name			= "vendor-moto",
 	.id			= GREYBUS_PROTOCOL_VENDOR,
-	.major			= 0,
-	.minor			= 1,
+	.major			= GB_VENDOR_MOTO_VERSION_MAJOR,
+	.minor			= GB_VENDOR_MOTO_VERSION_MINOR,
 	.connection_init	= gb_vendor_moto_connection_init,
 	.connection_exit	= gb_vendor_moto_connection_exit,
 	.request_recv		= NULL,	/* no incoming requests */
