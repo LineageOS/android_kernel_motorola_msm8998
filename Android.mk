8bcc9832c2b1e2d484835009f695ae27f26443fb
Kazuhiro Ondo <ondo@motorola.com>
2016-01-21 16:06:40 -0600

    V4L2 device driver kernel module for Mod Camera HAL
    This module exposes one video device (for Camera HAL) and one misc
    device to userspace. The misc device will be used by the camera daemon
    to control the video device and bridge V4L2 operations between HAL
    video device and GB video device/platform ISP. The video device will
    be used by Camera HAL to perform camera related operations over
    V4L2 APIs.
    
    Change-Id: Idcf5aedd6f03729b4ec405bf4ee38258739707c7
    Signed-off-by: Kazuhiro Ondo <ondo@motorola.com>
    Reviewed-on: http://gerrit.mot.com/817520
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

---
+++ Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := v4l2_hal
+LOCAL_MODULE_TAGS := optional
+LOCAL_ADDITIONAL_DEPENDENCIES := build-local
+include $(BUILD_PHONY_PACKAGE)
+
+LCL_SRC_PATH := $(LOCAL_PATH)
+LCL_KDIRARG := KERNELDIR="${ANDROID_PRODUCT_OUT}/obj/KERNEL_OBJ"
+ifeq ($(TARGET_ARCH),arm64)
+  LCL_KERNEL_TOOLS_PREFIX=aarch64-linux-android-
+else
+  LCL_KERNEL_TOOLS_PREFIX:=arm-eabi-
+endif
+LCL_ARCHARG := ARCH=$(TARGET_ARCH)
+LCL_FLAGARG := EXTRA_CFLAGS+=-fno-pic
+LCL_ARGS := $(LCL_KDIRARG) $(LCL_ARCHARG) $(LCL_FLAGARG)
+
+build-local: $(ACP) $(INSTALLED_KERNEL_TARGET)
+	$(MAKE) clean -C $(LCL_SRC_PATH)
+	$(MAKE) -j$(MAKE_JOBS) -C $(LCL_SRC_PATH) CROSS_COMPILE=$(LCL_KERNEL_TOOLS_PREFIX) $(LCL_ARGS)
+	ko=`find $(LCL_SRC_PATH) -type f -name "*.ko"`;\
+	for i in $$ko;\
+	do $(LCL_KERNEL_TOOLS_PREFIX)strip --strip-unneeded $$i;\
+	$(ACP) -fp $$i $(TARGET_OUT)/lib/modules/;\
+	done
---
+++ v4l2_hal.c
@@ -0,0 +1,584 @@
+/*
+ * Copyright (C) 2016 Motorola Mobility LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+#include <linux/compat.h>
+#include <linux/kernel.h>
+#include <linux/mutex.h>
+#include <linux/string.h>
+#include <media/v4l2-dev.h>
+#include <media/v4l2-device.h>
+#include <media/v4l2-ioctl.h>
+#include <media/videobuf2-core.h>
+
+#include "v4l2_hal.h"
+
+#define V4L2_HAL_DRIVER_VERSION 1
+#define HAL_DEV_NAME "v4l2_hal"
+
+struct v4l2_buffer_data {
+	int mapped_fd;
+};
+
+struct v4l2_stream_data {
+	unsigned int id;
+	bool used;
+	struct vb2_queue vb2_q;
+	struct v4l2_buffer_data *bdata;
+};
+
+struct v4l2_hal_data {
+	struct v4l2_device v4l2_dev;
+	struct video_device *vdev;
+	struct mutex lock;
+	struct v4l2_stream_data strms[V4L2_HAL_MAX_STREAMS];
+};
+
+struct v4l2_format_compat {
+	__u32 type;
+	union {
+		struct v4l2_pix_format	pix;
+		__u8	raw_data[200];		  /* user-defined */
+	} fmt;
+};
+
+#define VIDIOC_G_FMT32		_IOWR('V',	4, struct v4l2_format_compat)
+#define VIDIOC_S_FMT32		_IOWR('V',	5, struct v4l2_format_compat)
+
+static int query_cap(struct file *file, void *fh, struct v4l2_capability *cap)
+{
+	strlcpy(cap->driver, "v4l2 hal", sizeof(cap->driver));
+	strlcpy(cap->card, "v4l2 hal", sizeof(cap->card));
+	strlcpy(cap->bus_info, "greybus", sizeof(cap->card));
+	cap->version = V4L2_HAL_DRIVER_VERSION;
+	cap->device_caps = V4L2_CAP_VIDEO_CAPTURE;
+	cap->capabilities = cap->device_caps | V4L2_CAP_DEVICE_CAPS;
+	return 0;
+}
+
+static int input_enum(struct file *file, void *fh, struct v4l2_input *inp)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_ENUMINPUT,
+					sizeof(*inp), inp);
+
+	return ret;
+}
+
+static int input_get(struct file *file, void *fh, unsigned int *i)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_G_INPUT,
+					sizeof(*i), i);
+	return ret;
+}
+
+static int input_set(struct file *file, void *fh,
+			 unsigned int i)
+{
+	int ret;
+	unsigned int tmp = i;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_S_INPUT,
+					sizeof(tmp), &tmp);
+
+	return ret;
+}
+
+static int fmt_enum(struct file *file, void *fh,
+			struct v4l2_fmtdesc *fmt)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_ENUM_FMT,
+					sizeof(*fmt), fmt);
+
+		return ret;
+}
+
+static int fmt_get(struct file *file, void *fh,
+		   struct v4l2_format *fmt)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	/* G_FMT required special handling due to incompatiblity
+	   of v4l2_format structure */
+	if (v4l2_misc_compat_mode()) {
+		struct v4l2_format_compat cfmt;
+
+		cfmt.type = fmt->type;
+		memcpy(&cfmt.fmt, &fmt->fmt, sizeof(fmt->fmt));
+		ret = v4l2_misc_process_command(strm->id, VIDIOC_G_FMT32,
+						sizeof(cfmt), &cfmt);
+		fmt->type = cfmt.type;
+		memcpy(&fmt->fmt, &cfmt.fmt, sizeof(fmt->fmt));
+	} else
+		ret = v4l2_misc_process_command(strm->id, VIDIOC_G_FMT,
+						sizeof(*fmt), fmt);
+
+	return ret;
+}
+
+static int fmt_set(struct file *file, void *fh,
+		   struct v4l2_format *fmt)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	/* S_FMT required special handling due to incompatiblity
+	   of v4l2_format structure */
+	if (v4l2_misc_compat_mode()) {
+		struct v4l2_format_compat cfmt;
+
+		cfmt.type = fmt->type;
+		memcpy(&cfmt.fmt, &fmt->fmt, sizeof(fmt->fmt));
+		ret = v4l2_misc_process_command(strm->id, VIDIOC_S_FMT32,
+						sizeof(cfmt), &cfmt);
+		fmt->type = cfmt.type;
+		memcpy(&fmt->fmt, &cfmt.fmt, sizeof(fmt->fmt));
+	} else
+		ret = v4l2_misc_process_command(strm->id, VIDIOC_S_FMT,
+						sizeof(*fmt), fmt);
+
+	return ret;
+}
+
+static int frmsize_enum(struct file *file, void *fh,
+			struct v4l2_frmsizeenum *frmsize)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_ENUM_FRAMESIZES,
+					sizeof(*frmsize), frmsize);
+
+		return ret;
+}
+
+static int frmival_enum(struct file *file, void *fh,
+			struct v4l2_frmivalenum *frmival)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_ENUM_FRAMEINTERVALS,
+					sizeof(*frmival), frmival);
+
+		return ret;
+}
+
+
+static int stream_parm_get(struct file *file, void *fh,
+			   struct v4l2_streamparm *parm)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_G_PARM,
+					sizeof(*parm), parm);
+
+		return ret;
+}
+
+static int stream_parm_set(struct file *file, void *fh,
+			   struct v4l2_streamparm *parm)
+{
+	int ret;
+	struct v4l2_stream_data *strm = file->private_data;
+
+	ret = v4l2_misc_process_command(strm->id, VIDIOC_S_PARM,
+					sizeof(*parm), parm);
+
+	return ret;
+}
+
+static int request_bufs(struct file *file, void *fh,
+			struct v4l2_requestbuffers *req)
+{
+	struct v4l2_stream_data *strm = file->private_data;
+
+	return vb2_reqbufs(&strm->vb2_q, req);
+}
+
+static int queue_buf(struct file *file, void *fh, struct v4l2_buffer *b)
+{
+	struct v4l2_stream_data *strm = file->private_data;
+
+	return vb2_qbuf(&strm->vb2_q, b);
+}
+
+static int dequeue_buf(struct file *file, void *fh, struct v4l2_buffer *b)
+{
+	struct v4l2_stream_data *strm = file->private_data;
+
+	return vb2_dqbuf(&strm->vb2_q, b, file->f_flags & O_NONBLOCK);
+}
+
+static int streamon(struct file *file, void *fh, enum v4l2_buf_type buf_type)
+{
+	struct v4l2_stream_data *strm = file->private_data;
+
+	return vb2_streamon(&strm->vb2_q, buf_type);
+}
+
+static int streamoff(struct file *file, void *fh, enum v4l2_buf_type buf_type)
+{
+	struct v4l2_stream_data *strm = file->private_data;
+
+	return vb2_streamoff(&strm->vb2_q, buf_type);
+}
+
+static int query_ctrl(struct file *file, void *fh,
+			  struct v4l2_queryctrl *a)
+{
+	/* TODO: add code */
+	return 0;
+}
+
+static int get_ctrl(struct file *file, void *fh,
+			struct v4l2_control *a)
+{
+	/* TODO: add code */
+	return 0;
+}
+
+static int set_ctrl(struct file *file, void *fh,
+			struct v4l2_control *a)
+{
+	/* TODO: add code */
+	return 0;
+}
+
+static int v4l2_hal_queue_setup(struct vb2_queue *q,
+				const struct v4l2_format *fmt,
+				unsigned int *num_buffers,
+				unsigned int *num_planes,
+				unsigned int sizes[], void *alloc_ctxs[])
+{
+	struct v4l2_stream_data *strm = q->drv_priv;
+	struct v4l2_hal_reqbufs_data data;
+	struct v4l2_buffer_data *bdata;
+	int i;
+
+	*num_planes = 1;
+	/* The blow sets large enough number to get around of length check
+	   during qbuf */
+	sizes[0] = 0;
+
+	data.count = *num_buffers;
+
+	bdata = kmalloc_array(data.count, sizeof(*bdata), GFP_KERNEL);
+	if (bdata == NULL)
+		return -ENOMEM;
+
+	for (i = 0; i < data.count; i++)
+		bdata[i].mapped_fd = -1;
+
+	strm->bdata = bdata;
+
+	return v4l2_misc_process_command(strm->id, VIOC_HAL_STREAM_REQBUFS,
+					 sizeof(data), &data);
+}
+
+static void v4l2_hal_buf_queue(struct vb2_buffer *vb)
+{
+	struct v4l2_stream_data *strm = vb->vb2_queue->drv_priv;
+	struct v4l2_hal_qbuf_data data;
+
+	data.index = vb->v4l2_buf.index;
+	data.fd = (int)vb->v4l2_planes[0].m.userptr;
+	data.length = vb->v4l2_planes[0].length;
+
+	v4l2_misc_process_command(strm->id, VIOC_HAL_STREAM_QBUF,
+				  sizeof(data), &data);
+}
+
+static int v4l2_hal_start_streaming(struct vb2_queue *q, unsigned int count)
+{
+	struct v4l2_stream_data *strm = q->drv_priv;
+
+	return v4l2_misc_process_command(strm->id,
+					 VIOC_HAL_STREAM_ON, 0, NULL);
+}
+
+static void v4l2_hal_stop_streaming(struct vb2_queue *q)
+{
+	struct v4l2_stream_data *strm = q->drv_priv;
+
+	v4l2_misc_process_command(strm->id, VIOC_HAL_STREAM_OFF, 0, NULL);
+}
+
+static void *v4l2_hal_get_userptr(void *alloc_ctx,
+				  unsigned long vaddr,
+				  unsigned long size, int write)
+{
+	return (void *)vaddr;
+}
+
+static void v4l2_hal_put_userptr(void *buf_priv)
+{
+	/* Intentionally left empty. Required for user ptr mode mem operation */
+}
+static struct vb2_ops v4l2_hal_vb2_ops = {
+	.queue_setup = v4l2_hal_queue_setup,
+	.start_streaming = v4l2_hal_start_streaming,
+	.stop_streaming = v4l2_hal_stop_streaming,
+	.buf_queue = v4l2_hal_buf_queue,
+};
+
+static struct vb2_mem_ops v4l2_hal_vb2_mem_ops = {
+	.get_userptr = v4l2_hal_get_userptr,
+	.put_userptr = v4l2_hal_put_userptr,
+};
+
+static int v4l2_hal_vb2_q_init(struct v4l2_stream_data *strm,
+				   struct vb2_queue *q)
+{
+	memset(q, 0, sizeof(struct vb2_queue));
+	q->drv_priv = strm;
+	q->mem_ops = &v4l2_hal_vb2_mem_ops;
+	q->ops = &v4l2_hal_vb2_ops;
+
+	q->type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	q->io_modes = VB2_USERPTR;
+	q->io_flags = 0;
+	q->timestamp_flags = V4L2_BUF_FLAG_TIMESTAMP_MONOTONIC;
+	q->buf_struct_size = sizeof(struct vb2_buffer);
+
+	return vb2_queue_init(q);
+}
+
+int v4l2_hal_get_mapped_fd(void *hal_data, unsigned int stream, int index)
+{
+	struct v4l2_hal_data *data = hal_data;
+	struct v4l2_stream_data *strm;
+
+	strm = &data->strms[stream];
+	return strm->bdata[index].mapped_fd;
+}
+
+void v4l2_hal_set_mapped_fd(void *hal_data, unsigned int stream, int index,
+			    int fd)
+{
+	struct v4l2_hal_data *data = hal_data;
+	struct v4l2_stream_data *strm;
+
+	strm = &data->strms[stream];
+	strm->bdata[index].mapped_fd = fd;
+}
+
+int v4l2_hal_buffer_ready(void *hal_data, unsigned int stream, int index,
+			  unsigned int length)
+{
+	struct v4l2_hal_data *data = hal_data;
+	struct v4l2_stream_data *strm;
+	struct vb2_buffer *vb;
+
+	if (stream >= V4L2_HAL_MAX_STREAMS)
+		return -EINVAL;
+
+	strm = &data->strms[stream];
+	mutex_lock(&data->lock);
+	if (strm->used) {
+		vb = strm->vb2_q.bufs[index];
+		vb->v4l2_planes[0].bytesused = length;
+		vb2_buffer_done(vb, VB2_BUF_STATE_DONE);
+	}
+	mutex_unlock(&data->lock);
+
+	return 0;
+}
+
+static int v4l2_hal_open(struct file *file)
+{
+	struct v4l2_hal_data *data = video_drvdata(file);
+	unsigned long idx;
+	int ret;
+
+	/* This driver is counting on memory copy of structures btween
+	   tasks. Those task needs to be in same compat mode. */
+	if (v4l2_misc_compat_mode() != is_compat_task()) {
+		pr_err("compat mode mismatch. v4l2_hal: %d, misc: %d\n",
+			   is_compat_task(), v4l2_misc_compat_mode());
+		return -EFAULT;
+	}
+
+	mutex_lock(&data->lock);
+	for (idx = 0; idx < V4L2_HAL_MAX_STREAMS; idx++) {
+		if (!data->strms[idx].used) {
+			data->strms[idx].used = true;
+			break;
+		}
+	}
+	mutex_unlock(&data->lock);
+
+	if (idx == V4L2_HAL_MAX_STREAMS) {
+		pr_err("%s: No more stream cannot be opened\n", __func__);
+		return -EFAULT;
+	}
+
+	ret = v4l2_misc_process_command(idx, VIOC_HAL_STREAM_OPENED, 0, NULL);
+	if (ret) {
+		pr_err("%s: Stream open failed\n", __func__);
+		goto release_stream;
+	}
+
+	ret = v4l2_hal_vb2_q_init(&data->strms[idx], &data->strms[idx].vb2_q);
+	if (ret) {
+		pr_err("%s: Failed to initialize buffer queue\n", __func__);
+		goto close_misc;
+	}
+
+	file->private_data = &data->strms[idx];
+	return 0;
+
+close_misc:
+	v4l2_misc_process_command(idx, VIOC_HAL_STREAM_CLOSED, 0, NULL);
+
+release_stream:
+	mutex_lock(&data->lock);
+	data->strms[idx].used = false;
+	mutex_unlock(&data->lock);
+
+	return ret;
+}
+
+static int v4l2_hal_close(struct file *file)
+{
+	struct v4l2_hal_data *data = video_drvdata(file);
+	struct v4l2_stream_data *strm = file->private_data;
+
+	v4l2_misc_process_command(strm->id, VIOC_HAL_STREAM_CLOSED, 0, NULL);
+
+	vb2_queue_release(&strm->vb2_q);
+
+	mutex_lock(&data->lock);
+	strm->used = false;
+	kfree(strm->bdata);
+	mutex_unlock(&data->lock);
+
+	file->private_data = NULL;
+
+	return 0;
+}
+
+/* Callout ioctls not passing through */
+static const struct v4l2_ioctl_ops v4l2_hal_ioctl_ops = {
+	.vidioc_querycap				= query_cap,
+	.vidioc_enum_input				= input_enum,
+	.vidioc_g_input					= input_get,
+	.vidioc_s_input					= input_set,
+	.vidioc_enum_fmt_vid_cap		= fmt_enum,
+	.vidioc_g_fmt_vid_cap			= fmt_get,
+	.vidioc_s_fmt_vid_cap			= fmt_set,
+	.vidioc_enum_framesizes			= frmsize_enum,
+	.vidioc_enum_frameintervals		= frmival_enum,
+	.vidioc_g_parm					= stream_parm_get,
+	.vidioc_s_parm					= stream_parm_set,
+
+	.vidioc_reqbufs			= request_bufs,
+	.vidioc_qbuf			= queue_buf,
+	.vidioc_dqbuf			= dequeue_buf,
+
+	.vidioc_streamon		= streamon,
+	.vidioc_streamoff		= streamoff,
+
+	.vidioc_queryctrl		= query_ctrl,
+	.vidioc_g_ctrl			= get_ctrl,
+	.vidioc_s_ctrl			= set_ctrl,
+};
+
+static struct v4l2_file_operations v4l2_hal_fops = {
+	.owner	 = THIS_MODULE,
+	.open	 = v4l2_hal_open,
+	.ioctl	 = video_ioctl2,
+	.release = v4l2_hal_close,
+};
+
+void *v4l2_hal_init()
+{
+	int retval;
+	int i;
+	struct v4l2_hal_data *data;
+
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return NULL;
+
+	for (i = 0; i < V4L2_HAL_MAX_STREAMS; i++)
+		data->strms[i].id = i;
+
+	mutex_init(&data->lock);
+
+	snprintf(data->v4l2_dev.name, sizeof(data->v4l2_dev.name),
+		 "%s", HAL_DEV_NAME);
+
+	retval = v4l2_device_register(NULL, &data->v4l2_dev);
+	if (retval) {
+		pr_err("failed to register v4l2 hal\n");
+		goto error_reg_v4l2dev;
+	}
+
+	data->vdev = video_device_alloc();
+	if (data->vdev == NULL) {
+		pr_err("failed to allocate video_device hal\n");
+		goto error_alloc_vdev;
+	}
+
+	data->vdev->ctrl_handler = NULL;
+	data->vdev->v4l2_dev = &data->v4l2_dev;
+	data->vdev->release = video_device_release;
+	data->vdev->fops = &v4l2_hal_fops;
+	data->vdev->ioctl_ops = &v4l2_hal_ioctl_ops;
+	data->vdev->vfl_type = VFL_TYPE_GRABBER;
+
+	retval = video_register_device(data->vdev,
+					   VFL_TYPE_GRABBER, -1);
+	if (retval) {
+		pr_err("%s: failed to register video device. rc %d\n",
+			   __func__, retval);
+		goto error_reg_vdev;
+	}
+
+	video_set_drvdata(data->vdev, data);
+
+	return data;
+
+error_reg_vdev:
+	video_device_release(data->vdev);
+error_alloc_vdev:
+	v4l2_device_unregister(&data->v4l2_dev);
+error_reg_v4l2dev:
+	kfree(data);
+
+	return NULL;
+}
+
+void v4l2_hal_exit(void *data)
+{
+	struct v4l2_hal_data *hal_data = data;
+
+	if (data == NULL)
+		return;
+
+	video_unregister_device(hal_data->vdev);
+	v4l2_device_unregister(&hal_data->v4l2_dev);
+
+	kfree(data);
+}
