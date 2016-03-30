b23a34a25f7cb0d5d69f14b606ac77f1adba3fd8
Dmitriy Gruzman <dgruzman@motorola.com>
2016-03-30 15:26:41 -0500

    ptp: Report Mod current sourcing capabilities and availability
    Report if external or internal power is avaiable for consumption, the
    source of the power that is being supplied (battery, wired or wireless
    charger), and the maximum amount of current that can be drawn from the
    source.
    
    Update protocol version to 00.02.
    
    Change-Id: I7f9e7e120924a49c559f0a3e7c5a56eb129c0594
    Signed-off-by: Dmitriy Gruzman <dgruzman@motorola.com>
    Reviewed-on: http://gerrit.mot.com/839129
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    SME-Granted: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Tested-by: Jason Hrycay <jason.hrycay@motorola.com>

--- ptp.c
+++ ptp.c
@@ -26,7 +26,7 @@ struct gb_ptp {
 
 /* Version of the Greybus ptp protocol we support */
 #define	GB_PTP_VERSION_MAJOR		0x00
-#define	GB_PTP_VERSION_MINOR		0x01
+#define	GB_PTP_VERSION_MINOR		0x02
 
 /* Greybus ptp operation types */
 #define GB_PTP_TYPE_GET_FUNCTIONALITY		0x02
@@ -36,52 +36,92 @@ struct gb_ptp {
 #define GB_PTP_TYPE_EXT_POWER_PRESENT		0x06
 #define GB_PTP_TYPE_POWER_REQUIRED_CHANGED	0x07
 #define GB_PTP_TYPE_POWER_REQUIRED		0x08
+#define GB_PTP_TYPE_POWER_AVAILABLE_CHANGED	0x09 /* added in ver 00.02 */
+#define GB_PTP_TYPE_POWER_AVAILABLE		0x0A /* added in ver 00.02 */
+#define GB_PTP_TYPE_POWER_SOURCE		0x0B /* added in ver 00.02 */
+#define GB_PTP_TYPE_GET_MAX_OUTPUT_CURRENT	0x0C /* added in ver 00.02 */
 
-/* Mods internal source send power capabilities */
+/* Check for operation support */
+#define GB_PTP_SUPPORTS(p, name) \
+	((p->connection->module_major > GB_PTP_SUPPORT_##name##_MAJOR) || \
+	(p->connection->module_major == GB_PTP_SUPPORT_##name##_MAJOR && \
+	p->connection->module_minor >= GB_PTP_SUPPORT_##name##_MINOR))
+
+/* Operations added in ver 00.02 */
+#define GB_PTP_SUPPORT_POWER_AVAILABLE_CHANGED_MAJOR	0x00
+#define GB_PTP_SUPPORT_POWER_AVAILABLE_CHANGED_MINOR	0x02
+#define GB_PTP_SUPPORT_POWER_AVAILABLE_MAJOR		0x00
+#define GB_PTP_SUPPORT_POWER_AVAILABLE_MINOR		0x02
+#define GB_PTP_SUPPORT_POWER_SOURCE_MAJOR		0x00
+#define GB_PTP_SUPPORT_POWER_SOURCE_MINOR		0x02
+#define GB_PTP_SUPPORT_MAX_OUTPUT_CURRENT_MAJOR		0x00
+#define GB_PTP_SUPPORT_MAX_OUTPUT_CURRENT_MINOR		0x02
+
+/* Mod internal source send power capabilities */
 #define GB_PTP_INT_SND_NEVER		0x00
 #define GB_PTP_INT_SND_SUPPLEMENTAL	0x01
 #define GB_PTP_INT_SND_LOW_BATT_SAVER	0x02
 
-/* Mods internal source receive power capabilities */
+/* Mod internal source receive power capabilities */
 #define GB_PTP_INT_RCV_NEVER		0x00
 #define GB_PTP_INT_RCV_FIRST		0x01
 #define GB_PTP_INT_RCV_SECOND		0x02
 #define GB_PTP_INT_RCV_PARALLEL		0x03
 
-/* Mods external source capabilities */
+/* Mod external source capabilities */
 #define GB_PTP_EXT_NONE			0x00
 #define GB_PTP_EXT_SUPPORTED		0x01
 
-/* Current Flow Request */
+/* Current Flow Request from Phone to Mod */
 #define GB_PTP_CURRENT_OFF		0x00
 #define GB_PTP_CURRENT_TO_MOD		0x01
 #define GB_PTP_CURRENT_FROM_MOD		0x02
 
-/* External Power Present */
-#define GB_PTP_EXT_POWER_NOT_PRESENT	0x00
-#define GB_PTP_EXT_POWER_PRESENT	0x01
+/* Mod External Power Presence */
+#define GB_PTP_EXT_POWER_NOT_PRESENT		0x00
+#define GP_PTP_EXT_POWER_PRESENT		0x01 /* removed in ver 00.02 */
+#define GB_PTP_EXT_POWER_WIRELESS_PRESENT	0x02 /* added in ver 00.02 */
+#define GB_PTP_EXT_POWER_WIRED_PRESENT		0x03 /* added in ver 00.02 */
+#define GB_PTP_EXT_POWER_WIRED_WIRELESS_PRESENT	0x04 /* added in ver 00.02 */
 
-/* Mods internal source power requirements */
+/* Mod internal source power requirements */
 #define GB_PTP_POWER_NOT_REQUIRED	0x00
 #define GB_PTP_POWER_REQUIRED		0x01
+
+/* Mod power availability for Phone */
+#define GB_PTP_POWER_NOT_AVAILABLE	0x00
+#define GB_PTP_POWER_AVAILABLE_EXT	0x01
+#define GB_PTP_POWER_AVAILABLE_INT	0x02
+
+/* Mod power source supplying current to Phone*/
+#define GB_PTP_POWER_SOURCE_NONE	0x00
+#define GB_PTP_POWER_SOURCE_BATTERY	0x01
+#define GB_PTP_POWER_SOURCE_WIRED	0x02
+#define GB_PTP_POWER_SOURCE_WIRELESS	0x03
 
 static enum power_supply_property gb_ptp_props[] = {
 	POWER_SUPPLY_PROP_PTP_INTERNAL_SEND,
 	POWER_SUPPLY_PROP_PTP_INTERNAL_RECEIVE,
-	POWER_SUPPLY_PROP_PTP_INTERNAL_MAX_RECEIVE_VOLTAGE,
 	POWER_SUPPLY_PROP_PTP_EXTERNAL,
 	POWER_SUPPLY_PROP_PTP_CURRENT_FLOW,
 	POWER_SUPPLY_PROP_PTP_MAX_INPUT_CURRENT,
+	POWER_SUPPLY_PROP_PTP_MAX_OUTPUT_CURRENT,
 	POWER_SUPPLY_PROP_PTP_EXTERNAL_PRESENT,
 	POWER_SUPPLY_PROP_PTP_POWER_REQUIRED,
+	POWER_SUPPLY_PROP_PTP_POWER_AVAILABLE,
+	POWER_SUPPLY_PROP_PTP_POWER_SOURCE,
 };
 
 /* Greybus messages */
 struct gb_ptp_functionality_response {
 	__u8 int_snd;
 	__u8 int_rcv;
-	__le32 int_rcv_max_v;
+	__le32 unused;
 	__u8 ext;
+} __packed;
+
+struct gb_ptp_max_output_current_response {
+	__le32 curr;
 } __packed;
 
 struct gb_ptp_ext_power_present_response {
@@ -90,6 +130,14 @@ struct gb_ptp_ext_power_present_response {
 
 struct gb_ptp_power_required_response {
 	__u8 required;
+} __packed;
+
+struct gb_ptp_power_available_response {
+	__u8 available;
+} __packed;
+
+struct gb_ptp_power_source_response {
+	__u8 source;
 } __packed;
 
 struct gb_ptp_current_flow_request {
@@ -104,7 +152,6 @@ struct gb_ptp_max_input_current_request {
 struct gb_ptp_functionality {
 	int int_snd;
 	int int_rcv;
-	int int_rcv_max_v;
 	int ext;
 };
 
@@ -182,8 +229,15 @@ static int to_external_present_property(__u8 present)
 	case GB_PTP_EXT_POWER_NOT_PRESENT:
 		prop = POWER_SUPPLY_PTP_EXT_NOT_PRESENT;
 		break;
-	case GB_PTP_EXT_POWER_PRESENT:
-		prop = POWER_SUPPLY_PTP_EXT_PRESENT;
+	case GB_PTP_EXT_POWER_WIRED_PRESENT:
+		prop = POWER_SUPPLY_PTP_EXT_WIRED_PRESENT;
+		break;
+	case GP_PTP_EXT_POWER_PRESENT: /* default to wireless when unknown */
+	case GB_PTP_EXT_POWER_WIRELESS_PRESENT:
+		prop = POWER_SUPPLY_PTP_EXT_WIRELESS_PRESENT;
+		break;
+	case GB_PTP_EXT_POWER_WIRED_WIRELESS_PRESENT:
+		prop = POWER_SUPPLY_PTP_EXT_WIRED_WIRELESS_PRESENT;
 		break;
 	default:
 		prop = POWER_SUPPLY_PTP_EXT_PRESENCE_UNKNOWN;
@@ -212,6 +266,52 @@ static int to_power_required_property(__u8 required)
 	return prop;
 }
 
+static int to_power_available_property(__u8 available)
+{
+	int prop;
+
+	switch (available) {
+	case GB_PTP_POWER_NOT_AVAILABLE:
+		prop = POWER_SUPPLY_PTP_POWER_NOT_AVAILABLE;
+		break;
+	case GB_PTP_POWER_AVAILABLE_EXT:
+		prop = POWER_SUPPLY_PTP_POWER_AVAILABLE_EXTERNAL;
+		break;
+	case GB_PTP_POWER_AVAILABLE_INT:
+		prop = POWER_SUPPLY_PTP_POWER_AVAILABLE_INTERNAL;
+		break;
+	default:
+		prop = POWER_SUPPLY_PTP_POWER_AVAILABILITY_UNKNOWN;
+		break;
+	}
+
+	return prop;
+}
+
+static int to_power_source_property(__u8 source)
+{
+	int prop;
+
+	switch (source) {
+	case GB_PTP_POWER_SOURCE_NONE:
+		prop = POWER_SUPPLY_PTP_POWER_SOURCE_NONE;
+		break;
+	case GB_PTP_POWER_SOURCE_BATTERY:
+		prop = POWER_SUPPLY_PTP_POWER_SOURCE_BATTERY;
+		break;
+	case GB_PTP_POWER_SOURCE_WIRED:
+		prop = POWER_SUPPLY_PTP_POWER_SOURCE_WIRED;
+		break;
+	case GB_PTP_POWER_SOURCE_WIRELESS:
+		prop = POWER_SUPPLY_PTP_POWER_SOURCE_WIRELESS;
+		break;
+	default:
+		prop = POWER_SUPPLY_PTP_POWER_SOURCE_UNKNOWN;
+		break;
+	}
+
+	return prop;
+}
 
 static int gb_ptp_get_functionality(struct gb_ptp *ptp,
 				    struct gb_ptp_functionality *func)
@@ -225,9 +325,26 @@ static int gb_ptp_get_functionality(struct gb_ptp *ptp,
 
 	func->int_snd = to_internal_send_property(response.int_snd);
 	func->int_rcv = to_internal_receive_property(response.int_rcv);
-	func->int_rcv_max_v = le32_to_cpu(response.int_rcv_max_v);
 	func->ext = to_external_property(response.ext);
 
+	return 0;
+}
+
+static int gb_ptp_get_max_output_current(struct gb_ptp *ptp, int *curr)
+{
+	struct gb_ptp_max_output_current_response response;
+	int retval;
+
+	if (!GB_PTP_SUPPORTS(ptp, MAX_OUTPUT_CURRENT))
+		return -ENODEV;
+
+	retval = gb_operation_sync(ptp->connection,
+				   GB_PTP_TYPE_GET_MAX_OUTPUT_CURRENT, NULL, 0,
+				   &response, sizeof(response));
+	if (retval)
+		return retval;
+
+	*curr = le32_to_cpu(response.curr);
 	return 0;
 }
 
@@ -256,6 +373,46 @@ static int gb_ptp_power_required(struct gb_ptp *ptp, int *required)
 		return retval;
 
 	*required = to_power_required_property(response.required);
+	return 0;
+}
+
+static int gb_ptp_power_available(struct gb_ptp *ptp, int *available)
+{
+	struct gb_ptp_power_available_response response;
+	int retval;
+
+	if (!GB_PTP_SUPPORTS(ptp, POWER_AVAILABLE)) {
+		*available = POWER_SUPPLY_PTP_POWER_AVAILABILITY_UNKNOWN;
+		return 0;
+	}
+
+	retval = gb_operation_sync(ptp->connection,
+				   GB_PTP_TYPE_POWER_AVAILABLE, NULL, 0,
+				   &response, sizeof(response));
+	if (retval)
+		return retval;
+
+	*available = to_power_available_property(response.available);
+	return 0;
+}
+
+static int gb_ptp_power_source(struct gb_ptp *ptp, int *source)
+{
+	struct gb_ptp_power_source_response response;
+	int retval;
+
+	if (!GB_PTP_SUPPORTS(ptp, POWER_SOURCE)) {
+		*source = POWER_SUPPLY_PTP_POWER_SOURCE_UNKNOWN;
+		return 0;
+	}
+
+	retval = gb_operation_sync(ptp->connection,
+				   GB_PTP_TYPE_POWER_SOURCE, NULL, 0,
+				   &response, sizeof(response));
+	if (retval)
+		return retval;
+
+	*source = to_power_source_property(response.source);
 	return 0;
 }
 
@@ -296,12 +453,17 @@ static int gb_ptp_receive(u8 type, struct gb_operation *op)
 	struct gb_connection *connection = op->connection;
 	struct gb_ptp *ptp = connection->private;
 
-	if (type != GB_PTP_TYPE_EXT_POWER_CHANGED ||
-	    type != GB_PTP_TYPE_POWER_REQUIRED_CHANGED)
+	switch (type) {
+	case GB_PTP_TYPE_POWER_AVAILABLE_CHANGED:
+		if (!GB_PTP_SUPPORTS(ptp, POWER_AVAILABLE_CHANGED))
+			return -EINVAL;
+	case GB_PTP_TYPE_EXT_POWER_CHANGED:
+	case GB_PTP_TYPE_POWER_REQUIRED_CHANGED:
+		power_supply_changed(&ptp->psy);
+		return 0;
+	default:
 		return -EINVAL;
-
-	power_supply_changed(&ptp->psy);
-	return 0;
+	}
 }
 
 static int gb_ptp_get_property(struct power_supply *psy,
@@ -331,11 +493,6 @@ static int gb_ptp_get_property(struct power_supply *psy,
 		if (!retval)
 			val->intval = func.int_rcv;
 		break;
-	case POWER_SUPPLY_PROP_PTP_INTERNAL_MAX_RECEIVE_VOLTAGE:
-		retval = gb_ptp_get_functionality(ptp, &func);
-		if (!retval)
-			val->intval = func.int_rcv_max_v;
-		break;
 	case POWER_SUPPLY_PROP_PTP_EXTERNAL:
 		retval = gb_ptp_get_functionality(ptp, &func);
 		if (!retval)
@@ -345,11 +502,20 @@ static int gb_ptp_get_property(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_PTP_MAX_INPUT_CURRENT:
 		retval = -ENODEV; /* to make power_supply_uevent() happy */
 		break;
+	case POWER_SUPPLY_PROP_PTP_MAX_OUTPUT_CURRENT:
+		retval = gb_ptp_get_max_output_current(ptp, &val->intval);
+		break;
 	case POWER_SUPPLY_PROP_PTP_EXTERNAL_PRESENT:
 		retval = gb_ptp_ext_power_present(ptp, &val->intval);
 		break;
 	case POWER_SUPPLY_PROP_PTP_POWER_REQUIRED:
 		retval = gb_ptp_power_required(ptp, &val->intval);
+		break;
+	case POWER_SUPPLY_PROP_PTP_POWER_AVAILABLE:
+		retval = gb_ptp_power_available(ptp, &val->intval);
+		break;
+	case POWER_SUPPLY_PROP_PTP_POWER_SOURCE:
+		retval = gb_ptp_power_source(ptp, &val->intval);
 		break;
 	default:
 		retval = -EINVAL;
