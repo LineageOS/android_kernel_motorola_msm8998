080b0664ff7d56a550bddd005fb9eaf153e1d0c4
Sridhar Vashist <svashist@motorola.com>
2016-06-08 16:22:56 -0500

    camera_ext_v4l2: post uevent after adding open_mode attr
    send dev added uevent again after adding open_mode attr to fix
    the race condition of ueventd not being able to find open_mode.
    
    Mot CRs-fixed: IKKRNBSP-3949
    Change-Id: Icbbba390138afc9e63b5e2477990054d0d055d8f
    Signed-off-by: Sridhar Vashist <svashist@motorola.com>
    Reviewed-on: http://gerrit.mot.com/865362
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Kazuhiro Ondo <kazuhiro.ondo@motorola.com>
    Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- camera_ext_mod_v4l2.c
+++ camera_ext_mod_v4l2.c
@@ -825,6 +825,7 @@ int camera_ext_mod_v4l2_init(struct camera_ext *cam_dev)
 
 	camera_ext_get(cam_dev);
 	video_set_drvdata(cam_dev->vdev_mod, cam_dev);
+	kobject_uevent(&cam_dev->vdev_mod->dev.kobj, KOBJ_ADD);
 
 	return retval;
 error_sysfs:
