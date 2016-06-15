80b6e265ac4508fe7fefc8164e3a2a6391383544
Jason Hrycay <jason.hrycay@motorola.com>
2016-06-15 12:10:33 -0500

    mods_init: Cleanup on any init failures
    Whenever there is a failure during one of the initializations, ensure we
    cleanup the portions that completed successfully.
    
    Change-Id: Idd5a23a94fa47da9f7254c00c72dcc58d774349d
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: http://gerrit.mot.com/867466
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>
    Reviewed-by: Christopher Fries <cfries@motorola.com>
    Submit-Approved: Christopher Fries <cfries@motorola.com>
    Tested-by: Christopher Fries <cfries@motorola.com>

--- mods_init.c
+++ mods_init.c
@@ -33,18 +33,65 @@ struct dentry *mods_debugfs_get(void)
 
 static int __init mods_init(void)
 {
-	int err = 0;
+	int err;
 
 	mods_debug_root = debugfs_create_dir("mods", NULL);
+	if (!mods_debug_root)
+		pr_warn("failed to create 'mods' debugfs\n");
 
-	err |= muc_core_init();
-	err |= muc_svc_init();
-	err |= mods_ap_init();
-	err |= muc_spi_init();
-	err |= apba_ctrl_init();
-	err |= mods_uart_init();
+	err = muc_core_init();
+	if (err) {
+		pr_err("muc_core_init failed: %d\n", err);
+		goto exit;
+	}
 
-	return (err ? -ENODEV : 0);
+	err = muc_svc_init();
+	if (err) {
+		pr_err("muc_svc_init failed: %d\n", err);
+		goto svc_fail;
+	}
+
+	err = mods_ap_init();
+	if (err) {
+		pr_err("mods_ap_init failed: %d\n", err);
+		goto ap_fail;
+	}
+
+	err = muc_spi_init();
+	if (err) {
+		pr_err("muc_spi_init failed: %d\n", err);
+		goto spi_fail;
+	}
+
+	err = apba_ctrl_init();
+	if (err) {
+		pr_err("apba_ctrl_init failed: %d\n", err);
+		goto apba_fail;
+	}
+
+	err = mods_uart_init();
+	if (err) {
+		pr_err("mods_uart_init failed: %d\n", err);
+		goto uart_fail;
+	}
+
+	return 0;
+
+uart_fail:
+	apba_ctrl_exit();
+apba_fail:
+	muc_spi_exit();
+spi_fail:
+	mods_ap_exit();
+ap_fail:
+	muc_svc_exit();
+svc_fail:
+	muc_core_exit();
+exit:
+	debugfs_remove_recursive(mods_debug_root);
+	mods_debug_root = NULL;
+
+	return err;
 }
 
 static void __exit mods_exit(void)
