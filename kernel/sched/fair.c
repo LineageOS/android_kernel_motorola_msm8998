4f47c74c26e2e372ef5ed84385129f5dc1697fa3
Joonwoo Park <joonwoop@codeaurora.org>
2016-10-21 13:39:31 -0700

    sched/fair: prevent meaningless active migration
    At present need_active_balance() determines whether an active
    upmigration is needed by using capacity_of(). A CPU's capacity
    may be reduced by RT pressure, and therefore distinguishing
    capability differences with capacity_of() may lead to suboptimal
    active migrations to less capable CPUs. Use capacity_orig_of
    to distinguish differently capable CPUs in addition to
    capacity_of(), thus avoiding placing tasks on less capable CPUs
    due to instantaneous RT pressure.
    
    Change-Id: I3e1435246a8edc3ad618ef98a34866cfbd8c16a5
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    [markivx: Reworked the commit text a bit]
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>
    
    (cherry picked from commit e818c7f7fb70c60067c4a4f652327d8275c569dc)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10073,6 +10073,7 @@ static int need_active_balance(struct lb_env *env)
 
 	if (energy_aware() &&
 	    (capacity_of(env->src_cpu) < capacity_of(env->dst_cpu)) &&
+	    ((capacity_orig_of(env->src_cpu) < capacity_orig_of(env->dst_cpu))) &&
 				env->src_rq->cfs.h_nr_running == 1 &&
 				cpu_overutilized(env->src_cpu) &&
 				!cpu_overutilized(env->dst_cpu)) {
