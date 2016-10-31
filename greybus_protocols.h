33dd5adc32f7905e74f8c78444ee4c36523bdad6
Jason Hrycay <jason.hrycay@motorola.com>
2016-10-31 13:17:57 -0500

    sensors-ext: Move defines/structs to common header
    Move all of the message identifiers and structures to the common
    greybus_protocols.h header so that a single header file can be used for
    retrieving the various definitions needed to implement the protocol.
    This can be used by user-space such as a greybus simulator.
    
    Change-Id: I4ce8e70d91bf23056b90314240733c83c6b341b1
    Signed-off-by: Jason Hrycay <jason.hrycay@motorola.com>
    Reviewed-on: https://gerrit.mot.com/915175
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Reviewed-by: Gregory Meiste <w30289@motorola.com>

--- greybus_protocols.h
+++ greybus_protocols.h
@@ -1861,5 +1861,62 @@ struct gb_usb_ext_attach_request {
 
 /* no data for gb_usb_ext_attach_response */
 
+/* SENSORS EXT */
+
+/* Version of the Greybus protocol we support */
+#define	GB_SENSORS_EXT_VERSION_MAJOR		0x00
+#define	GB_SENSORS_EXT_VERSION_MINOR		0x02
+
+/* Greybus Motorola vendor specific request types */
+#define GB_SENSORS_EXT_TYPE_SENSOR_COUNT	0x02
+#define GB_SENSORS_EXT_TYPE_SENSOR_INFO		0x03
+#define GB_SENSORS_EXT_TYPE_START_REPORTING	0x04
+#define GB_SENSORS_EXT_TYPE_FLUSH		0x05
+#define GB_SENSORS_EXT_TYPE_STOP_REPORTING	0x06
+#define GB_SENSORS_EXT_TYPE_EVENT		0x07
+
+/* get count of sensors in module */
+struct gb_sensors_get_sensor_count_response {
+	__u8	sensors_count;
+} __packed;
+
+struct gb_sensors_ext_sensor_info_request {
+	__u8	sensor_id;
+} __packed;
+/* sensor info response structure is gb_sensor */
+
+/* this request has no response payload */
+struct gb_sensors_ext_start_reporting_request {
+	__u8	sensor_id;
+	__u8	reserved[3];
+	__le64	sampling_period;
+	__le64	max_report_latency;
+} __packed;
+
+/* this request has no response payload */
+struct gb_sensors_ext_flush_request {
+	__u8	sensor_id;
+} __packed;
+
+struct gb_sensors_ext_report_data {
+	__u8	sensor_id;
+	__u8	flags;
+	__le16	readings;
+	__le64	reference_time;
+	__u8	reading[];
+} __packed;
+
+struct gb_sensors_ext_report_hdr {
+	__u8	reporting_sensors_count;
+	__u8	reserved;
+	struct gb_sensors_ext_report_data sensor[];
+} __packed;
+
+
+/* this request has no response payload */
+struct gb_sensors_ext_stop_reporting_request {
+	__u8	sensor_id;
+} __packed;
+
 #endif /* __GREYBUS_PROTOCOLS_H */
 
--- sensors_ext.h
+++ sensors_ext.h
@@ -14,61 +14,7 @@
 #ifndef _SENSORS_EXT_H
 #define _SENSORS_EXT_H
 
-/* Version of the Greybus protocol we support */
-#define	GB_SENSORS_EXT_VERSION_MAJOR		0x00
-#define	GB_SENSORS_EXT_VERSION_MINOR		0x02
-
-/* Greybus Motorola vendor specific request types */
-#define GB_SENSORS_EXT_TYPE_SENSOR_COUNT	0x02
-#define GB_SENSORS_EXT_TYPE_SENSOR_INFO		0x03
-#define GB_SENSORS_EXT_TYPE_START_REPORTING	0x04
-#define GB_SENSORS_EXT_TYPE_FLUSH		0x05
-#define GB_SENSORS_EXT_TYPE_STOP_REPORTING	0x06
-#define GB_SENSORS_EXT_TYPE_EVENT		0x07
-
-
-/* get count of sensors in module */
-struct gb_sensors_get_sensor_count_response {
-	__u8	sensors_count;
-} __packed;
-
-struct gb_sensors_ext_sensor_info_request {
-	__u8	sensor_id;
-} __packed;
-/* sensor info response structure is gb_sensor */
-
-/* this request has no response payload */
-struct gb_sensors_ext_start_reporting_request {
-	__u8	sensor_id;
-	__u8	reserved[3];
-	__le64	sampling_period;
-	__le64	max_report_latency;
-} __packed;
-
-/* this request has no response payload */
-struct gb_sensors_ext_flush_request {
-	__u8	sensor_id;
-} __packed;
-
-struct gb_sensors_ext_report_data {
-	__u8	sensor_id;
-	__u8	flags;
-	__le16	readings;
-	__le64	reference_time;
-	__u8	reading[];
-} __packed;
-
-struct gb_sensors_ext_report_hdr {
-	__u8	reporting_sensors_count;
-	__u8	reserved;
-	struct gb_sensors_ext_report_data sensor[];
-} __packed;
-
-
-/* this request has no response payload */
-struct gb_sensors_ext_stop_reporting_request {
-	__u8	sensor_id;
-} __packed;
+#include "greybus_protocols.h"
 
 int gb_sensors_ext_start_reporting(uint8_t sensor_id,
 	uint64_t sampling_period, uint64_t max_report_latency);
