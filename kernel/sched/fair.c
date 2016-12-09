04f24d96f10f24794cf841f663e55972c07fdc72
Joonwoo Park <joonwoop@codeaurora.org>
2016-12-08 16:12:12 -0800

    sched: EAS/WALT: use cr_avg instead of prev_runnable_sum
    WALT accounts two major statistics; CPU load and cumulative tasks
    demand.
    
    The CPU load which is account of accumulated each CPU's absolute
    execution time is for CPU frequency guidance.  Whereas cumulative
    tasks demand which is each CPU's instantaneous load to reflect
    CPU's load at given time is for task placement decision.
    
    Use cumulative tasks demand for cpu_util() for task placement and
    introduce cpu_util_freq() for frequency guidance.
    
    Change-Id: Id928f01dbc8cb2a617cdadc584c1f658022565c5
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    (cherry picked from commit e48e4c23e830d0e9938f4160920aa81c5972bdd2)

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5827,7 +5827,7 @@ static inline void hrtick_update(struct rq *rq)
 static bool cpu_overutilized(int cpu);
 unsigned long boosted_cpu_util(int cpu);
 #else
-#define boosted_cpu_util(cpu) cpu_util(cpu)
+#define boosted_cpu_util(cpu) cpu_util_freq(cpu)
 #endif
 
 #if defined(CONFIG_SMP) && defined(CONFIG_CPU_FREQ_GOV_SCHED)
@@ -7064,7 +7064,7 @@ schedtune_task_margin(struct task_struct *task)
 unsigned long
 boosted_cpu_util(int cpu)
 {
-	unsigned long util = cpu_util(cpu);
+	unsigned long util = cpu_util_freq(cpu);
 	long margin = schedtune_cpu_margin(util, cpu);
 
 	trace_sched_boost_cpu(cpu, util, margin);
