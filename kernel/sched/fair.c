7124345ac4b3489680ae583a91ef651e4102c214
Suren Baghdasaryan <surenb@google.com>
2017-08-30 14:29:11 -0700

    Revert "sched/fair: kick nohz idle balance for misfit task"
    This reverts commit c9b653282bda72e7ca4d2592a4f6dd1bf1f27b40.
    
    bug: 64952298
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: Id0fa42b850defedca430f28e35b6831a3321b155

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11316,10 +11316,6 @@ static inline int _nohz_kick_needed(struct rq *rq, int cpu, int *type)
 	    (!energy_aware() || cpu_overutilized(cpu)))
 	    	return true;
 
-	/* Do idle load balance if there have misfit task */
-	if (energy_aware() && rq->misfit_task)
-		return true;
-
 	return (rq->nr_running >= 2);
 }
 
