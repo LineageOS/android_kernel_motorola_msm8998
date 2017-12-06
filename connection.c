59346204e4224375ac8ca12e1d6394246b48e328
Ashok Oliver Prabhu <ashok@motorola.com>
2017-12-06 06:19:22 -0600

    Revert "(CR): greybus: fine tune attached cport disconnect logic"
    This reverts commit 9b503ab2b0bdd34bf45ca94f82bac6dd3cc6d77f.
    
    Change-Id: I78fee70b085b546b6a58518a9815072d7fc46b33
    Reviewed-on: https://gerrit.mot.com/1100317
    SME-Granted: SME Approvals Granted
    Reviewed-by: Ashok Oliver Prabhu <ashok@motorola.com>
    SLTApproved: Ashok Oliver Prabhu <ashok@motorola.com>
    Submit-Approved: Ashok Oliver Prabhu <ashok@motorola.com>
    Tested-by: Ashok Oliver Prabhu <ashok@motorola.com>

--- connection.c
+++ connection.c
@@ -479,8 +479,7 @@ void gb_connection_exit(struct gb_connection *connection)
 		GB_CONNECTION_STATE_DESTROYING : GB_CONNECTION_STATE_ATTACHED_DESTROYING;
 	spin_unlock_irq(&connection->lock);
 
-	if (connection->state == GB_CONNECTION_STATE_DESTROYING)
-		gb_connection_cancel_operations(connection, -ESHUTDOWN);
+	gb_connection_cancel_operations(connection, -ESHUTDOWN);
 
 	connection->protocol->connection_exit(connection);
 	gb_connection_control_disconnected(connection);
