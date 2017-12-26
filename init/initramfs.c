97abec18cbf555ec868e69b19feb5ef230c11f53
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-25 21:11:50 -0600

    [HAX][DO NOT MERGE] init: swap recovery/rom boots
    * Can be usefull during bringups. When told to boot to recovery,
      a device will attempt to boot the rom and vice-versa. Saves you
      from having to manually select recovery after a failed rom boot.
    
    Change-Id: I545f37bbabedd4cbe4c8fbf57ed0fffd773d0464

--- init/initramfs.c
+++ init/initramfs.c
@@ -621,7 +621,7 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (do_skip_initramfs)
+	if (!do_skip_initramfs)
 		return default_rootfs();
 
 	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
