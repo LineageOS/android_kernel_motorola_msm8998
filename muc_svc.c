c9ebe6e6655b301b131e7413010583dcacc3b19c
briandai <e50300@motorola.com>
2018-01-30 16:34:02 -0600

    external: greybus: muc_svc: disable version heartbeat
    Version heartbeat check is only used in muc_svc_communication_reset
    function but it does not involve any communication cross the wire to
    MOD side. This version check will return successful result even after
    MOD side software crashed. This will prevent the communication reset
    for more than 2 minutes until "RDY assert" failed at physical layer.
    By removing this check, the MOD can get reset to recover from the
    failure state in about 3 sec or after 3 consecutive greybus timeout.
    
    Change-Id: I9b59c1c7308da037038934561b0705bfcc16fb25
    Reviewed-on: https://gerrit.mot.com/1128506
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- muc_svc.c
+++ muc_svc.c
@@ -94,7 +94,9 @@ static void muc_svc_broadcast_slave_notification(struct mods_dl_device *master);
 static int muc_svc_send_reboot(struct mods_dl_device *mods_dev, uint8_t mode);
 static int muc_svc_send_current_limit(struct mods_dl_device *dev, uint8_t limit);
 static int muc_svc_send_current_rsv_ack(struct mods_dl_device *dev);
+#ifdef ENABLE_VERSION_HEARTBEAT
 static int muc_svc_version_heartbeat(void);
+#endif
 static int muc_svc_send_rtc_sync(struct mods_dl_device *mods_dev);
 static int muc_svc_send_test_mode(struct mods_dl_device *mods_dev, uint32_t val);
 
@@ -256,9 +258,11 @@ muc_svc_attach(struct notifier_block *nb, unsigned long state, void *unused)
 
 void muc_svc_communication_reset(struct mods_dl_device *error_dev)
 {
+#ifdef ENABLE_VERSION_HEARTBEAT
 	/* Try a heartbeat via the version; if it succeeds we are talking */
 	if (!muc_svc_version_heartbeat())
 		return;
+#endif
 
 	dev_err(&svc_dd->pdev->dev, "%s: resetting via interface: %d\n",
 		__func__, error_dev->intf_id);
@@ -1832,6 +1836,7 @@ static int muc_svc_control_version(struct mods_dl_device *mods_dev, u8 type,
 	return 0;
 }
 
+#ifdef ENABLE_VERSION_HEARTBEAT
 static int muc_svc_version_heartbeat(void)
 {
 	struct mods_dl_device *mods_dev;
@@ -1857,6 +1862,7 @@ static int muc_svc_version_heartbeat(void)
 
 	return ret;
 }
+#endif
 
 static int
 muc_svc_get_manifest(struct mods_dl_device *mods_dev, uint16_t out_cport)
