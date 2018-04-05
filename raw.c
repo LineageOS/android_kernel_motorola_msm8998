4d2dda1cb998c8b27c70b64d58e27977e43257a7
Sridhar Vashist <svashist@motorola.com>
2018-04-05 15:23:29 -0500

    greybus: wrap packetbus driver around raw
    packetbus is a raw based protocol to all mod mgr to reoute packets based
    on port numbers.
    
    CRs-fixed: (CR)
    
    Change-Id: I2673f33c89cd5190e1e49e56429b9dadd34eec54
    Signed-off-by: Sridhar Vashist <svashist@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1165731
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Ravikumar Vembu <raviv@motorola.com>

--- raw.c
+++ raw.c
@@ -46,7 +46,9 @@ struct raw_data {
 };
 
 static struct class *raw_class;
+static struct class *pbsys_class;
 static int raw_major;
+static int pbsys_major;
 static const struct file_operations raw_fops;
 static DEFINE_IDA(minors);
 static DEFINE_SPINLOCK(raw_lock);
@@ -188,7 +190,7 @@ static int gb_raw_send(struct gb_raw *raw, u32 len, const char __user *data)
 	request->len = cpu_to_le32(len);
 
 	retval = gb_operation_unidirectional(connection, GB_RAW_TYPE_SEND,
-					     request, len + sizeof(*request));
+					 request, len + sizeof(*request));
 
 	kfree(request);
 	return retval;
@@ -206,7 +208,7 @@ static int gb_raw_connection_init(struct gb_connection *connection)
 {
 	struct gb_raw *raw;
 	int retval;
-	int minor;
+	int minor = -1;
 
 	raw = kzalloc(sizeof(*raw), GFP_KERNEL);
 	if (!raw)
@@ -220,28 +222,36 @@ static int gb_raw_connection_init(struct gb_connection *connection)
 	INIT_LIST_HEAD(&raw->list);
 	mutex_init(&raw->list_lock);
 
-	minor = ida_simple_get(&minors, 0, 0, GFP_KERNEL);
-	if (minor < 0) {
-		retval = minor;
-		goto error_free;
-	}
-
 	raw->device = kzalloc(sizeof(*raw->device), GFP_KERNEL);
 	if (!raw->device) {
 		retval = -ENOMEM;
-		goto error_devalloc;
+		goto error_free;
 	}
 
-	raw->device->devt = MKDEV(raw_major, minor);
-	raw->device->class = raw_class;
-	raw->device->parent = &connection->bundle->dev;
-	raw->device->release = gb_raw_dev_release;
-	dev_set_name(raw->device, "gbraw%d", minor);
+	if (connection->protocol_id == GREYBUS_PROTOCOL_PACKETBUS) {
+		raw->device->devt = MKDEV(pbsys_major, 0);
+		raw->device->class = pbsys_class;
+		raw->device->parent = &connection->bundle->dev;
+		raw->device->release = gb_raw_dev_release;
+		dev_set_name(raw->device, "gbsys");
+	} else {
+		minor = ida_simple_get(&minors, 0, 0, GFP_KERNEL);
+		if (minor < 0) {
+			retval = minor;
+			goto error_free;
+		}
+		raw->device->devt = MKDEV(raw_major, minor);
+		raw->device->class = raw_class;
+		raw->device->parent = &connection->bundle->dev;
+		raw->device->release = gb_raw_dev_release;
+		dev_set_name(raw->device, "gbraw%d", minor);
+	}
+
 	device_initialize(raw->device);
 
 	init_waitqueue_head(&raw->read_wq);
 	atomic_set(&raw->open_excl, 0);
-	raw->dev = MKDEV(raw_major, minor);
+	raw->dev = raw->device->devt;
 	cdev_init(&raw->cdev, &raw_fops);
 	raw->cdev.kobj.parent = &raw->device->kobj;
 	retval = cdev_add(&raw->cdev, raw->dev, 1);
@@ -262,12 +272,12 @@ error_device:
 
 error_cdev:
 	put_device(raw->device);
-	kfree(raw->device);
-error_devalloc:
-	ida_simple_remove(&minors, minor);
+	if (minor > 0)
+		ida_simple_remove(&minors, minor);
 
 error_free:
 	gb_connection_put(raw->connection);
+	kfree(raw->device);
 	kfree(raw);
 	return retval;
 }
@@ -296,6 +306,16 @@ static void gb_raw_connection_exit(struct gb_connection *connection)
 static struct gb_protocol raw_protocol = {
 	.name			= "raw",
 	.id			= GREYBUS_PROTOCOL_RAW,
+	.major			= GB_RAW_VERSION_MAJOR,
+	.minor			= GB_RAW_VERSION_MINOR,
+	.connection_init	= gb_raw_connection_init,
+	.connection_exit	= gb_raw_connection_exit,
+	.request_recv		= gb_raw_receive,
+};
+
+static struct gb_protocol packetbus_protocol = {
+	.name			= "pb",
+	.id			= GREYBUS_PROTOCOL_PACKETBUS,
 	.major			= GB_RAW_VERSION_MAJOR,
 	.minor			= GB_RAW_VERSION_MINOR,
 	.connection_init	= gb_raw_connection_init,
@@ -358,8 +378,8 @@ static ssize_t raw_read(struct file *file, char __user *buf, size_t count,
 			do {
 				mutex_unlock(&raw->list_lock);
 				retval = wait_event_interruptible(raw->read_wq,
-				    !list_empty(&raw->list) ||
-				    raw->state == GB_RAW_DESTROYED);
+				!list_empty(&raw->list) ||
+				raw->state == GB_RAW_DESTROYED);
 
 				if (retval < 0)
 					return retval;
@@ -429,33 +449,51 @@ static const struct file_operations raw_fops = {
 
 static int raw_init(void)
 {
-	dev_t dev;
+	dev_t dev_raw;
+	dev_t dev_pbsys;
+
 	int retval;
 
 	raw_class = class_create(THIS_MODULE, "gb_raw");
-	if (IS_ERR(raw_class)) {
-		retval = PTR_ERR(raw_class);
-		goto error_class;
+	pbsys_class = class_create(THIS_MODULE, "gb_sys");
+
+	if (IS_ERR(raw_class)||IS_ERR(pbsys_class)) {
+		retval = PTR_ERR(raw_class)|PTR_ERR(pbsys_class);
+		goto error_chrdev;
 	}
 
-	retval = alloc_chrdev_region(&dev, 0, NUM_MINORS, "gb_raw");
+	retval = alloc_chrdev_region(&dev_raw, 0, NUM_MINORS, "gb_raw");
 	if (retval < 0)
 		goto error_chrdev;
 
+	retval = alloc_chrdev_region(&dev_pbsys, 0, NUM_MINORS, "gb_sys");
+	if (retval < 0) {
+		unregister_chrdev_region(dev_raw, NUM_MINORS);
+		goto error_chrdev;
+	}
 
-	raw_major = MAJOR(dev);
+	raw_major = MAJOR(dev_raw);
+	pbsys_major = MAJOR(dev_pbsys);
 
 	retval = gb_protocol_register(&raw_protocol);
 	if (retval)
 		goto error_gb;
 
+	retval = gb_protocol_register(&packetbus_protocol);
+	if (retval) {
+		gb_protocol_deregister(&raw_protocol);
+		goto error_gb;
+	}
+
 	return 0;
 
 error_gb:
-	unregister_chrdev_region(dev, NUM_MINORS);
+	unregister_chrdev_region(dev_raw, NUM_MINORS);
+	unregister_chrdev_region(dev_pbsys, NUM_MINORS);
 error_chrdev:
 	class_destroy(raw_class);
-error_class:
+	class_destroy(pbsys_class);
+
 	return retval;
 }
 module_init(raw_init);
@@ -463,6 +501,9 @@ module_init(raw_init);
 static void __exit raw_exit(void)
 {
 	gb_protocol_deregister(&raw_protocol);
+	gb_protocol_deregister(&packetbus_protocol);
+	unregister_chrdev_region(MKDEV(pbsys_major, 0), NUM_MINORS);
+	class_destroy(pbsys_class);
 	unregister_chrdev_region(MKDEV(raw_major, 0), NUM_MINORS);
 	class_destroy(raw_class);
 	ida_destroy(&minors);
