eb1d52bf80b827a5677b138e8b7bf2192457d53a
Paul Keith <javelinanddart@gmail.com>
2018-10-19 00:45:37 +0200

    Revert "video: mdss: LiveDisplay driver"
    * We don't need this for CABC
    
    This reverts commit 6032360e82ef904e74fa59b71506b2dbe9f6deec.
    
    Change-Id: Iaa1058e582ba8449a69f51432750348ecb7b8c95

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -56,8 +56,6 @@
 #include "mdss_smmu.h"
 #include "mdss_mdp.h"
 
-#include "mdss_livedisplay.h"
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -1303,7 +1301,7 @@ static int mdss_fb_create_sysfs(struct msm_fb_data_type *mfd)
 		pr_err("panel parameter sysfs creation failed, rc=%d\n", rc);
 
 err:
-	return mdss_livedisplay_create_sysfs(mfd);
+	return rc;
 }
 
 static void mdss_fb_remove_sysfs(struct msm_fb_data_type *mfd)
