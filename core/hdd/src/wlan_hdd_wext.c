MotorolaMobilityLLC/vendor-qcom-opensource-wlan-qcacld-3.0

Implement WE_GET_MCC_MODE IOCTL for 8998
To check if MCC Mode is enabled or disabled in ini file,
implemented WE_GET_MCC_MODE that is being used in frameworks.
For dual band devices, this need to be enabled.

Change-Id: Ibe675d80a484687d950037c449718fc9ff2677c1
Reviewed-on: https://gerrit.mot.com/945510
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Binesh Balasingh <binesh@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

core/hdd/src/wlan_hdd_wext.c
@@ -1084,6 +1084,7 @@ static const hdd_freq_chan_map_t freq_chan_map[] = {
  * </ioctl>
  */
 #define WE_GET_CONCURRENCY_MODE 9
+#define WE_GET_MCC_MODE 10 /* MOTOROLA IKLOCSEN-2877 */
 /*
  * <ioctl>
  * get_nss - Get the number of spatial STBC streams (NSS)
@@ -8951,6 +8952,14 @@ static int __iw_setnone_getint(struct net_device *dev,
 		break;
 	}
 
+    // BEGIN MOTOROLA IKLOCSEN-2877, Get configured value of MCC mode
+    case WE_GET_MCC_MODE:
+    {
+        *value = (int)hdd_is_mcc_mode_enabled();
+		hdd_notice("MCC mode=%d", *value);
+        break;
+    } // END IKLOCSEN-2877
+
 	case WE_GET_NSS:
 	{
 		sme_get_config_param(hHal, &smeConfig);
@@ -12997,6 +13006,12 @@ static const struct iw_priv_args we_private_args[] = {
 	 0,
 	 IW_PRIV_TYPE_INT | IW_PRIV_SIZE_FIXED | 1,
 	 "getconcurrency"},
+
+    /* MOTOROLA IKLOCSEN-2877 */
+    {WE_GET_MCC_MODE,
+     0,
+	 IW_PRIV_TYPE_INT | IW_PRIV_SIZE_FIXED | 1,
+	 "getMccMode"},
 
 	{WE_GET_NSS,
 	 0,

LineageOS/android_kernel_motorola_msm8998

core/hdd/src/wlan_hdd_wext.c
@@ -1120,6 +1120,7 @@
  * </ioctl>
  */
 #define WE_GET_CONCURRENCY_MODE 9
+#define WE_GET_MCC_MODE 10 /* MOTOROLA IKLOCSEN-2877 */
 /*
  * <ioctl>
  * get_nss - Get the number of spatial STBC streams (NSS)
@@ -5554,6 +5555,14 @@ static int __iw_setnone_getint(struct net_device *dev,
 		break;
 	}
 
+	// BEGIN MOTOROLA IKLOCSEN-2877, Get configured value of MCC mode
+	case WE_GET_MCC_MODE:
+	{
+		*value = (int)hdd_is_mcc_mode_enabled();
+		hdd_debug("MCC mode=%d", *value);
+		break;
+	} // END IKLOCSEN-2877
+
 	case WE_GET_NSS:
 	{
 		sme_get_config_param(mac_handle, sme_config);
@@ -9547,6 +9556,12 @@ static const struct iw_priv_args we_private_args[] = {
 	 0,
 	 IW_PRIV_TYPE_INT | IW_PRIV_SIZE_FIXED | 1,
 	 "getconcurrency"},
+
+	/* MOTOROLA IKLOCSEN-2877 */
+	{WE_GET_MCC_MODE,
+	 0,
+	 IW_PRIV_TYPE_INT | IW_PRIV_SIZE_FIXED | 1,
+	 "getMccMode"},
 
 	{WE_GET_NSS,
 	 0,